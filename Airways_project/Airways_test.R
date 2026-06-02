632cd65c068f537f6691b70b6fbd2d697624214d
Jamilasullivan <sullivan-al-kadhomiyj@cardiff.ac.uk>
2026-06-02 23:42:31 +0100

    Beginning DESeq2 biological inference.

--- Airways_project/Airways_test.R
+++ Airways_project/Airways_test.R
@@ -29,4 +29,55 @@ head(rownames(airway)) # what does each row represent?
 
 colnames(airway) # what does each column represent?
 
+## plotting ####################################################################
+
+counts <- assay(airway) # extracts the number of genes matched to each gene
+
+library_sizes <- colSums(counts) # sum of reads for each sample (sequencing depth)
+
+barplot(
+  library_sizes,
+  las = 2,
+  main = "Reads per sample",
+  ylab = "Total counts"
+)
+
+library_sizes <- colSums(assay(airway)) # see if read numbers correlate to treatment groups
+
+data.frame(
+  Sample = colnames(airway),
+  Treatment = colData(airway)$dex,
+  Reads = library_sizes
+) # no correlation
+
+summary(library_sizes) # very large difference between the largest and smallest (requires normalisation)
+library_sizes
+
+## DESeq2 for normalisation ####################################################
+
+dds <- DESeqDataSet(airway, design = ~ cell + dex) # Creates a DESeq2 object with the airway data set and specifying the study's design. 
+dds <- estimateSizeFactors(dds) # how large the library is on average (>1 = above average or smaller library than normal). Use by DESeq2 to normalise counts.
+
+sizeFactors(dds) # used for technical scaling corrections
+
+# DESeq2 uses 'normalized count = raw count / size factor' internally with these numbers.
+
+# Do samples now cluster by treatment?
+
+## PCA plotting ################################################################
+
+vsd <- vst(dds)
+plotPCA(vsd, intgroup = "dex") # shows treated vs untreated samples. they separate clearly on PC1, so treatment is the strongest driver of variation.
+
+## finding the genes that cause the separation #################################
+
+dds <- DESeq(dds)
+res <- results(dds)
+
+
+
+
+
+
+
 
